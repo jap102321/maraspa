faf7748559d76d559a011beb9eabc9ff1563876d
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-07-09 17:45:16 -0500

    Removing promotion(temporary)

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="*" />
         </Routes>
-        <Promotion />
+        {/* <Promotion /> */}
         <Footer />
       </Router>
     </>
