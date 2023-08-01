ec863d3243aaefa0d8b38a74632135f79fdabd27
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-08-01 09:24:04 -0500

    Adding promotion august

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import { createGlobalStyle } from "styled-components";
-import Header from "./components/Header";
-// import Promotion from "./components/Promotion";
-import Footer from "./components/Footer";
-import BonoRegalo from "./pages/BonoRegalo";
-import Services from "./pages/Services";
-import Service from "./pages/Service/Service";
-import Contact from "./pages/Contact";
-import Instalations from "./pages/Instalations";
-import Outstanding from "./pages/outstanding/Outstanding";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import { createGlobalStyle } from 'styled-components';
+import Header from './components/Header';
+import Promotion from './components/Promotion';
+import Footer from './components/Footer';
+import BonoRegalo from './pages/BonoRegalo';
+import Services from './pages/Services';
+import Service from './pages/Service/Service';
+import Contact from './pages/Contact';
+import Instalations from './pages/Instalations';
+import Outstanding from './pages/outstanding/Outstanding';
 
 const GlobalStyle = createGlobalStyle`
 
@@ -34,19 +34,19 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/giftbono" element={<BonoRegalo />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/services/facial-clean" element={<Service />} />
-          <Route path="/services/packages" element={<Service />} />
-          <Route path="/services/massages" element={<Service />} />
-          <Route path="/services/steaming-bath" element={<Outstanding />} />
-          <Route path="/services/shaping-massage" element={<Outstanding />} />
-          <Route path="/instalations" element={<Instalations />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" />
+          <Route path='/' element={<Home />} />
+          <Route path='/giftbono' element={<BonoRegalo />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/services/facial-clean' element={<Service />} />
+          <Route path='/services/packages' element={<Service />} />
+          <Route path='/services/massages' element={<Service />} />
+          <Route path='/services/steaming-bath' element={<Outstanding />} />
+          <Route path='/services/shaping-massage' element={<Outstanding />} />
+          <Route path='/instalations' element={<Instalations />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' />
         </Routes>
-        {/* <Promotion /> */}
+        <Promotion />
         <Footer />
       </Router>
     </>
--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -1,24 +1,25 @@
-import styles from "../styles/Promotion.module.css";
-import imgPromo from "../assets/Imagenes/promo.webp";
-import Button from "./UI/Button";
-import { Link } from "react-router-dom";
+import styles from '../styles/Promotion.module.css';
+import imgPromo from '../assets/Imagenes/promo.webp';
+import Button from './UI/Button';
+import { Link } from 'react-router-dom';
 const Promotion = () => {
   return (
     <>
-      <div className={styles.promotion} data-aos="zoom-in-up">
-        <img src={imgPromo} alt="Limpieza facial bono de regalo" />
+      <div className={styles.promotion} data-aos='zoom-in-up'>
+        <img src={imgPromo} alt='Limpieza facial bono de regalo' />
         <div className={styles.promo}>
-          <h2>Promoción Julio</h2>
+          <h2>Promoción Agosto</h2>
           <div className={styles.description}>
-            <h3>Válido hasta: 15 Julio 2023</h3>
+            <h3>Válido para 2 personas</h3>
             <h3 id={styles.description}>
-              Aprovecha esta promoción que tenemos de julio
+              Invita a tu persona favorita a un momento de descanso y
+              tranquilidad, por el mes de agosto esta promoción paga uno van dos
             </h3>
           </div>
           <div className={styles.otherInfo}>
-            <h3>Costo de 130.000$</h3>
-            <Link to="/contact">
-              <Button title="Conoce más" theme="cardButton" />
+            <h3>Costo de 170.000$</h3>
+            <Link to='/contact'>
+              <Button title='Conoce que incluye' theme='cardButton' />
             </Link>
           </div>
         </div>
