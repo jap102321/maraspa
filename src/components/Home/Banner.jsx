1f4e9aa9386bf765bf841e173f1744ff46f99f1c
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-09-22 19:30:46 -0500

    Changing titles to SEO improve

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,14 +1,14 @@
-import styles from "../../styles/Banner.module.css";
-import Button from "../UI/Button";
-import { Link } from "react-router-dom";
+import styles from '../../styles/Banner.module.css';
+import Button from '../UI/Button';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
   return (
     <>
-      <div className={styles.banner} data-aos="fade-left">
+      <div className={styles.banner} data-aos='fade-left'>
         <div className={styles.description}>
-          <h2 style={{ fontFamily: "Parisienne, sans-serif" }}>MaraSpa</h2>
-          <h1>Relaja tu cuerpo, libera tu mente</h1>
+          <h1 style={{ fontFamily: 'Parisienne, sans-serif' }}>MaraSpa</h1>
+          <h2>Relaja tu cuerpo, libera tu mente</h2>
           <p>
             En cada uno de nuestros masajes, llevamos a cabo una exhaustiva
             evaluación de tus necesidades para brindarte un servicio
@@ -16,8 +16,8 @@ const Banner = () => {
             complementados con la inestimable experiencia de nuestras masajistas
             expertas que te atenderán.
           </p>
-          <Link to="/services">
-            <Button title="Servicios" theme="bannerBtn" data-aos="fade-left" />
+          <Link to='/services'>
+            <Button title='Servicios' theme='bannerBtn' data-aos='fade-left' />
           </Link>
         </div>
         <div className={styles.bgImage}></div>
--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -1,15 +1,15 @@
 import styles from '../styles/Promotion.module.css';
-import vidPromo from '../assets/videos/sepprom.mp4';
-// import imgPromo from '../assets/Imagenes/promo.webp';
+// import vidPromo from '../assets/videos/sepprom.mp4';
+import imgPromo from '../assets/Imagenes/promo.jpg';
 import Button from './UI/Button';
 import { Link } from 'react-router-dom';
 const Promotion = () => {
   return (
     <>
       <div className={styles.promotion} data-aos='zoom-in-up'>
-        <video src={vidPromo} controls='controls' />
+        <img src={imgPromo} alt='Limpieza facial bono de regalo' />
         <Link to='/contact'>
-          <Button title='Conoce que incluye' theme='cardButton'/>
+          <Button title='Conoce que incluye' theme='cardButton' />
         </Link>
       </div>
     </>
@@ -17,23 +17,3 @@ const Promotion = () => {
 };
 
 export default Promotion;
-
-{
-  /* <img src={imgPromo} alt='Limpieza facial bono de regalo' />
-<div className={styles.promo}>
-  <h2>Promoción Agosto</h2>
-  <div className={styles.description}>
-    <h3>Paga 1 y van 2 personas.</h3>
-    <h3 id={styles.description}>
-      Masaje relajante con piedras volcanicas, exfoliación corporal y
-      baño de cajón.
-    </h3>
-  </div>
-  <div className={styles.otherInfo}>
-    <h3>Costo de 170.000$</h3>
-    <Link to='/contact'>
-      <Button title='Conoce que incluye' theme='cardButton' />
-    </Link>
-  </div>
-</div> */
-}
